fa61d801a7837c0c53311da188491e0708401567
Lena Feld <lena.feld@student.kit.edu>
2020-06-08 10:16:55 +0200

    requirements

--- setup.py
+++ setup.py
@@ -4,4 +4,13 @@ setup(
     name="contre",
     author="Lena Feld",
     author_email="lena.feld@student.kit.edu",
-    description="CONTinuum REweighting for Belle and Belle II")
+    description="CONTinuum REweighting for Belle and Belle II",
+    install_requires=[
+        "basf2_mva",
+        "b2luigi",
+        "Sphinx",
+        "root-pandas",
+        "scikit-learn",
+        "numpy",
+        "jupyter"],
+    )
