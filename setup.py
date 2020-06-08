3d9601f42d684f8cac4eca5cd54e5e8844854f67
Lena Feld <lena.feld@student.kit.edu>
2020-06-08 10:38:24 +0200

    remove basf2 from requirement

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     author_email="lena.feld@student.kit.edu",
     description="CONTinuum REweighting for Belle and Belle II",
     install_requires=[
-        "basf2_mva",
         "b2luigi",
         "Sphinx",
+        "pandas",
         "root-pandas",
         "scikit-learn",
         "numpy",
