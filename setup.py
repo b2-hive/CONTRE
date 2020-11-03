9176861400fcc5887d8a8944ee2c636ba09aa239
Lena Feld <lena.feld@student.kit.edu>
2020-11-03 17:36:36 +0100

    fix package imports setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="contre",
+    packages=find_packages(),
     author="Lena Feld",
     author_email="lena.feld@student.kit.edu",
     description="CONTinuum REweighting for Belle and Belle II",
     install_requires=[
         "b2luigi",
-        "Sphinx",
         "pandas",
         "root-pandas",
         "scikit-learn",
